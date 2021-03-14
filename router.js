0e1ba92cf8da8d6cc0731cae4d995f46991ddba3
lsh <fantasyz7@naver.com>
2021-03-14 17:06:56 +0900

    getbyId 404 test

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const controllerType1 = require("./controller/type1");
 router.get("/", controllerType1.getTypes);
+router.get("/:typeId", controllerType1.getTypesById);
 router.post("/", controllerType1.createTypes);
 
 module.exports = router;
