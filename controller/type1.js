bf01c8ad72aa8b7abd7b6531e0dcdccaddd4c850
lsh <fantasyz7@naver.com>
2021-06-05 15:12:58 +0900

    json return check implemented

--- controller/type1.js
+++ controller/type1.js
@@ -5,6 +5,7 @@
 const type1Model = require("../models/type1");
 
 exports.createProduct = (req, res, next) => {
-  type1Model.create(req.body);
-  res.status(201).send();
+  const created = type1Model.create(req.body);
+  res.status(201).json(created);
+  // res.status(201).send();
 };
