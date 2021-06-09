966cbdfaee65b49de936fdd64f81a59811758dcb
lsh <fantasyz7@naver.com>
2021-06-09 23:43:06 +0900

    get data ctrl created

--- controller/type1.js
+++ controller/type1.js
@@ -4,7 +4,7 @@
 // };
 const type1Model = require("../models/type1");
 
-exports.createProduct = async (req, res, next) => {
+exports.createTypes = async (req, res, next) => {
   try {
     const created = await type1Model.create(req.body);
     console.log(`created:${created}`);
@@ -24,4 +24,18 @@ exports.getTypes = async (req, res, next) => {
     next(error);
   }
   // res.status(200).send();
+};
+
+exports.getTypesById = async (req, res, next) => {
+  const type = await type1Model.findById(req.params.typeId);
+  try {
+    if (type) {
+      res.status(200).json(type);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
--- router.js
+++ router.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const controllerType1 = require("./controller/type1");
-router.get("/", controllerType1.createProduct);
-router.post("/", controllerType1.createProduct);
+router.get("/", controllerType1.getTypes);
+router.post("/", controllerType1.createTypes);
 
 module.exports = router;
